962b76ce7227443dae09eb23115e4d128c8c5f51
Padure Gheorghe <Padure Gheorghe@DESKTOP-N2VEKE8>
2018-02-19 09:33:41 +0100

    Install CreateWeb+ Project

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.home')
+
+@section('content')
+    <div class="index-container container animated slideInUp">
+        <div class="jumbotron">
+            <h2>Hello, world!</h2>
+            <p>...</p>
+            <p ><a class="btn btn-default btn-sm" href="#" role="button" >Learn more</a></p>
+        </div>
+    </div>
+@endsection
